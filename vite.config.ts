741f68f787609952dfaf58fd4e381883a51a4139
Kim Jung Won <wjd7569@naver.com>
2026-02-25 06:42:07 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+      },
+    },
   }
 });
